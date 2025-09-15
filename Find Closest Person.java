9b1647e945b6cec6c44e45e7641fd34bf620eb0f
NITHISH <133564248+Nithishs-v@users.noreply.github.com>
2025-09-15 13:32:40 +0530

    Create Find Closest Person.java

---
+++ Find Closest Person.java
@@ -0,0 +1,22 @@
+[  ] Amla 
+[  ] Pomegranate 
+[  ] Banana
+[  ] Black channa
+[  ] Sweet potato 
+[  ] Carrot juice
+[  ] Beetroot juice 
+[  ] Carrot +orange  juice
+[  ] Eggs
+[  ] Makanana 
+[  ] Moongdal spourts 
+[  ] Groundnut 
+[  ] Badam
+[  ] Pisata
+[  ] Walnuts
+[  ] Chicken
+[  ] Flax seed
+[  ] Chia seed
+[  ] Pumpkin seed 
+[  ] Sunflower seed
+[  ] Avacodo 
+[  ] Rasins
