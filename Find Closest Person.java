7aa053aae7fc1f54b30dd5c845af9f9705cab9cc
NITHISH <133564248+Nithishs-v@users.noreply.github.com>
2025-09-15 13:33:03 +0530

    Update Find Closest Person.java

--- Find Closest Person.java
+++ Find Closest Person.java
@@ -1,22 +1,13 @@
-[  ] Amla 
-[  ] Pomegranate 
-[  ] Banana
-[  ] Black channa
-[  ] Sweet potato 
-[  ] Carrot juice
-[  ] Beetroot juice 
-[  ] Carrot +orange  juice
-[  ] Eggs
-[  ] Makanana 
-[  ] Moongdal spourts 
-[  ] Groundnut 
-[  ] Badam
-[  ] Pisata
-[  ] Walnuts
-[  ] Chicken
-[  ] Flax seed
-[  ] Chia seed
-[  ] Pumpkin seed 
-[  ] Sunflower seed
-[  ] Avacodo 
-[  ] Rasins
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int d1 = Math.abs(x - z);
+        int d2 = Math.abs(y - z);
+        
+        if (d1 < d2)
+            return 1;
+        else if (d2 < d1)
+            return 2;
+        else
+            return 0;
+    }
+}
